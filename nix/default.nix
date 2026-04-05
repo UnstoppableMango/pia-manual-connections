16f571e9976a6e419a188302fc1d6d52ec5165c4
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-04-04 21:44:41 -0500

    Docker and proper script references

--- nix/default.nix
+++ nix/default.nix
@@ -1,5 +1,5 @@
 {
-  buildEnv,
+  busybox,
   curl,
   fetchFromGitHub,
   iproute2,
@@ -13,59 +13,73 @@
   makeWrapper,
 }:
 let
-  packageScript =
-    name:
-    stdenv.mkDerivation {
-      pname = "pia-manual-connections";
-      inherit version;
-
-      nativeBuildInputs = [ makeWrapper ];
-
-      src = fetchFromGitHub {
-        owner = "pia-foss";
-        repo = "manual-connections";
-        rev = "a1412dbe2ca41edbb79c766bc475335cb6cb13ad";
-        sha256 = "sha256-SaCxirr/LF2a8PdTTZY98aEYVCYC0RxfT1XpWs4x7f0=";
-      };
-
-      patches = [
-        ../patches/001-echo-token.patch
-        ../patches/002-wireguard-script-args.patch
-      ];
-
-      installPhase = ''
-        install -Dm755 ${name}.sh $out/bin/${name}
-      '';
-
-      postFixup = ''
-        wrapProgram $out/bin/${name} --set PATH ${
-          lib.makeBinPath [
-            curl
-            iproute2
-            jq
-            ncurses
-            openvpn
-            wireguard-tools
-          ]
-        }
-      '';
-
-      meta = {
-        description = "Manual connections for PIA";
-        homepage = "https://github.com/pia-foss/manual-connections";
-        license = lib.licenses.mit;
-        maintainers = with lib.maintainers; [ UnstoppableMango ];
-      };
-    };
-in
-buildEnv {
-  name = "pia-manual-connections";
-  paths = [
-    (packageScript "connect_to_openvpn_with_token")
-    (packageScript "connect_to_wireguard_with_token")
-    (packageScript "get_region")
-    (packageScript "get_token")
-    (packageScript "port_forwarding")
-    (packageScript "run_setup")
+  path = lib.makeBinPath [
+    busybox
+    curl
+    iproute2
+    jq
+    ncurses
+    openvpn
+    wireguard-tools
   ];
+in
+stdenv.mkDerivation {
+  pname = "pia-manual-connections";
+  inherit version;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  src = fetchFromGitHub {
+    owner = "pia-foss";
+    repo = "manual-connections";
+    rev = "a1412dbe2ca41edbb79c766bc475335cb6cb13ad";
+    sha256 = "sha256-SaCxirr/LF2a8PdTTZY98aEYVCYC0RxfT1XpWs4x7f0=";
+  };
+
+  patches = [
+    ../patches/001-echo-token.patch
+    ../patches/002-wireguard-script-args.patch
+  ];
+
+  installPhase = ''
+    install -Dm755 connect_to_openvpn_with_token.sh $out/bin/connect_to_openvpn_with_token
+    install -Dm755 connect_to_wireguard_with_token.sh $out/bin/connect_to_wireguard_with_token
+    install -Dm755 get_token.sh $out/bin/get_token
+    install -Dm755 get_region.sh $out/bin/get_region
+    install -Dm755 port_forwarding.sh $out/bin/port_forwarding
+    install -Dm755 run_setup.sh $out/bin/run_setup
+  '';
+
+  postFixup = ''
+    substituteInPlace $out/bin/connect_to_openvpn_with_token \
+      --replace ./port_forwarding.sh $out/bin/port_forwarding
+    substituteInPlace $out/bin/connect_to_openvpn_with_token \
+      --replace ./get_token.sh $out/bin/get_token
+
+    substituteInPlace $out/bin/connect_to_wireguard_with_token \
+      --replace ./port_forwarding.sh $out/bin/port_forwarding
+    substituteInPlace $out/bin/connect_to_wireguard_with_token \
+      --replace ./get_token.sh $out/bin/get_token
+
+    substituteInPlace $out/bin/get_region \
+      --replace ./connect_to_openvpn_with_token.sh $out/bin/connect_to_openvpn_with_token
+    substituteInPlace $out/bin/get_region \
+      --replace ./connect_to_wireguard_with_token.sh $out/bin/connect_to_wireguard_with_token
+    substituteInPlace $out/bin/get_region \
+      --replace ./get_token.sh $out/bin/get_token
+
+    substituteInPlace $out/bin/run_setup \
+      --replace ./get_region.sh $out/bin/get_region
+
+    wrapProgram $out/bin/connect_to_openvpn_with_token --set PATH ${path}
+    wrapProgram $out/bin/connect_to_wireguard_with_token --set PATH ${path}
+    wrapProgram $out/bin/run_setup --set PATH ${path}
+  '';
+
+  meta = {
+    description = "Manual connections for PIA";
+    homepage = "https://github.com/pia-foss/manual-connections";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ UnstoppableMango ];
+  };
 }
